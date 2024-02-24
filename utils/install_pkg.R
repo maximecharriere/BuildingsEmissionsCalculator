dafe27f26f45f3b96882308fc89868a191e56fad
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2024-02-24 21:58:51 +0100

    Update

--- utils/install_pkg.R
+++ utils/install_pkg.R
@@ -1,25 +1,3 @@
-#' pkg_install
-#'
-#' Install one or more package from a given CRAN repository,
-#' is they're not already installed.
-#'
-#' @param packages Vector of packages to install.
-#' @param repos Link to the CRAN repo to get the packages.
-#'
-#' @examples
-#' pkg_install(packages = c("knitr", "readxl"), repos = "https://stat.ethz.ch/CRAN")
-pkg_install <- function(packages, repos = "https://stat.ethz.ch/CRAN") {
-  packagecheck <- match(packages, utils::installed.packages()[, 1])
-  packagestoinstall <- packages[is.na(packagecheck)]
-  if (length(packagestoinstall) > 0L) {
-    utils::install.packages(packagestoinstall,
-      repos = repos
-    )
-  } else {
-    print("All requested packages already installed")
-  }
-}
-
 pkg_install(packages = c(
   "tidyverse" #
   , "languageserver" # Language server for R
