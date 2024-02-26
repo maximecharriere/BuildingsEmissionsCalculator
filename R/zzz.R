20892b1993dc090845fa32fb62c1bacd556e5b6f
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2024-02-26 18:40:42 +0100

    Move data

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+.onLoad <- function(libname, pkgname) {
+  # Create a new environment to store the constants
+  .constants <- new.env(parent = emptyenv())
+
+  # Path to the constants.json file within the package
+  constants_path <- system.file("extdata", "constants.json", package = pkgname)
+
+  # Load the JSON file using jsonlite
+  constants <- jsonlite::fromJSON(constants_path)
+
+  # Assign the loaded constants to the .Constants environment
+  list2env(constants, envir = .constants)
+
+  # Assign the .Constants environment to the package namespace
+  assign(".constants", .constants, envir = asNamespace(pkgname))
+}
