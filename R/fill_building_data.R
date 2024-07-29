1868461d032cb3c0f3ee879872faa3bf7c60c9db
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2024-07-29 18:43:04 +0200

    test openxlsx2

--- R/fill_building_data.R
+++ R/fill_building_data.R
@@ -16,52 +16,54 @@
 #' building_data <- list(EGID = "123456")
 #' enriched_building_data <- fill_building_data(building_data)
 fill_building_data <- function(building, data_source = "web", sqlite_conn = NULL) {
-  building <- tryCatch({
-    library(co2calculatorPACTA2022) # TODO found why the script is not working if I remove this line
-    
-    # Request GeoAdmin to get the EGID
-    if (is.na(building$EGID)) {
-      building <- egid_search(building)
-    }
+  building <- tryCatch(
+    {
+      library(co2calculatorPACTA2022) # TODO found why the script is not working if I remove this line
 
-    # Request regbl to get the building data
-    if (data_source == "sqlite") {
-      building <- request_regbl_sqlite(building, sqlite_conn = sqlite_conn)
-    } else if (data_source == "web") {
-      building <- request_regbl_web(building)
-    } else {
-      stop("Invalid RegBl data source. Please specify either 'sqlite' or 'web'.")
-    }
+      # Request GeoAdmin to get the EGID
+      # if (is.na(building$EGID)) {
+      #   building <- egid_search(building)
+      # }
 
-    # If the building is demolished, stop the process
-    if (!(is.na(building$GABBJ))) {
-      stop("Building was demolished in ", building$GABBJ, ".")
-    }
-
-    # Convert the building data from the regbl format to the sia format
-    building <- regbl_2_sia_converter(building)
-
-    # Calculate the CO2 emissions
-    tryCatch({
-        emissions <- co2calculatorPACTA2022::calculate_emissions(
-          area = building$energy_relevant_area,
-          floors = building$floors,
-          year = building$year,
-          utilisation_key = building$utilisation_key,
-          climate_code = building$climate_code,
-          energy_carrier = building$energy_carrier,
-          walls_refurb_year = building$walls_refurb_year,
-          roof_refurb_year = building$roof_refurb_year,
-          windows_refurb_year = building$windows_refurb_year,
-          floor_refurb_year = building$floor_refurb_year,
-          heating_install_year = building$heating_install_year
-        )
-        # Update the emissions to the building data
-        building[names(emissions)] <- emissions
-      },
-      error = function(e) {
-        stop("Error from co2calculator: ", e$message)
+      # Request regbl to get the building data
+      if (data_source == "sqlite") {
+        building <- request_regbl_sqlite(building, sqlite_conn = sqlite_conn)
+      } else if (data_source == "web") {
+        building <- request_regbl_web(building)
+      } else {
+        stop("Invalid RegBl data source. Please specify either 'sqlite' or 'web'.")
       }
+
+      # If the building is demolished, stop the process
+      if (!(is.na(building$GABBJ))) {
+        stop("Building was demolished in ", building$GABBJ, ".")
+      }
+
+      # Convert the building data from the regbl format to the sia format
+      building <- regbl_2_sia_converter(building)
+
+      # Calculate the CO2 emissions
+      tryCatch(
+        {
+          emissions <- co2calculatorPACTA2022::calculate_emissions(
+            area = building$energy_relevant_area,
+            floors = building$floors,
+            year = building$year,
+            utilisation_key = building$utilisation_key,
+            climate_code = building$climate_code,
+            energy_carrier = building$energy_carrier,
+            walls_refurb_year = building$walls_refurb_year,
+            roof_refurb_year = building$roof_refurb_year,
+            windows_refurb_year = building$windows_refurb_year,
+            floor_refurb_year = building$floor_refurb_year,
+            heating_install_year = building$heating_install_year
+          )
+          # Update the emissions to the building data
+          building[names(emissions)] <- emissions
+        },
+        error = function(e) {
+          stop("Error from co2calculator: ", e$message)
+        }
       )
       # message("Building ", building$EGID, " filled.")
       return(building)
@@ -70,7 +72,8 @@ fill_building_data <- function(building, data_source = "web", sqlite_conn = NULL
       building$error_comments <- append_error_message(building$error_comments, e$message)
       message("Error on EGID ", building$EGID, ": ", e$message)
       return(building)
-    })
+    }
+  )
   return(building)
 }
 
@@ -127,7 +130,7 @@ fill_buildings_df <- function(buildings_df, data_source = "web", regbl_db_path =
     # Set SQLite database parameters for performance optimization
     RSQLite::dbExecute(sqlite_conn, "PRAGMA synchronous = OFF;")
     RSQLite::dbExecute(sqlite_conn, "PRAGMA journal_mode = WAL;")
-    RSQLite::dbExecute(sqlite_conn, "PRAGMA cache_size = -1000000;") #1GB of memory used for cache
+    RSQLite::dbExecute(sqlite_conn, "PRAGMA cache_size = -1000000;") # 1GB of memory used for cache
     RSQLite::dbExecute(sqlite_conn, "PRAGMA temp_store = MEMORY;")
   }
 
--- inst/scripts/install_pkg.R
+++ inst/scripts/install_pkg.R
@@ -28,7 +28,7 @@ pkg_install <- function(packages, repos = "https://stat.ethz.ch/CRAN", force_upd
     if (pkg %in% rownames(installed)) {
       installed_version <- as.character(installed[pkg, "Version"])
       available_version <- as.character(available[pkg, "Version"])
-      
+
       # Check if we need to update the package
       if (force_update && available_version > installed_version) {
         return(pkg)
@@ -56,22 +56,23 @@ pkg_install <- function(packages, repos = "https://stat.ethz.ch/CRAN", force_upd
 # --------------------- #
 
 pkg_install(packages = c(
-  "tidyverse",       # Data manipulation and visualization
-  "languageserver",  # Language server for R, provides IDE features
-  "writexl",         # Write data frames to Excel files
-  "readxl",          # Read Excel files
-  "jsonlite",        # Read and write JSON data
-  "roxygen2",        # Generate documentation from inline comments
-  "pkgdown",         # Generate a website for your package
-  "devtools",        # Tools to make package development easier
-  "usethis",         # Automate package and project setup
-  "RSQLite",         # Interface to SQLite databases
-  "httr2",           # Modern HTTP client for R
-  "xml2",            # Work with XML files
-  "ids",             # Generate unique identifiers
-  "future",          # Support for concurrent and parallel R programming
-  "future.apply",    # Apply functions in parallel using 'future'
-  "knitr",           # Dynamic report generation in R
-  "htmltools",       # Tools for HTML generation and rendering
-  "progressr",       # Progress updates in the R console
+  "tidyverse", # Data manipulation and visualization
+  "languageserver", # Language server for R, provides IDE features
+  "writexl", # Write data frames to Excel files
+  "readxl", # Read Excel files
+  "jsonlite", # Read and write JSON data
+  "roxygen2", # Generate documentation from inline comments
+  "pkgdown", # Generate a website for your package
+  "devtools", # Tools to make package development easier
+  "usethis", # Automate package and project setup
+  "RSQLite", # Interface to SQLite databases
+  "httr2", # Modern HTTP client for R
+  "xml2", # Work with XML files
+  "ids", # Generate unique identifiers
+  "future", # Support for concurrent and parallel R programming
+  "future.apply", # Apply functions in parallel using 'future'
+  "knitr", # Dynamic report generation in R
+  "htmltools", # Tools for HTML generation and rendering
+  "progressr", # Progress updates in the R console
+  "openxlsx2" # Read and write Excel files
 ), repos = "https://stat.ethz.ch/CRAN", force_update = TRUE)
